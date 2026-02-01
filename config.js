93e52c74acf3a8369c66be52f4f3b58a35ce6120
php56611-ship-it <php56611@gmail.com>
2026-02-01 02:55:02 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    // Token do bot
+    token: "MTQ2MzMwNDA4NjMyNDY0NTk2MA.G5Rhse.BHv4cX9eWQfuWnel-Pw6Wzb7VYRGRhf-Zvd8O4",
+    
+    // ID do servidor onde o bot funcionará
+    serverId: "1409356256212287561",
+    
+    // ID do cargo de staff
+    staffRoleId: "1440925265797709885",
+    
+    // ID da categoria onde os tickets serão criados
+    ticketCategoryId: "1441200192861769809",
+    
+    // ID do canal onde os logs serão enviados
+    logsChannelId: "1440900988952383669",
+    
+    // Estado do sistema de tickets (true = ligado, false = desligado)
+    ticketSystemEnabled: true,
+    
+    // ID do dono do bot
+    botOwnerId: "1459462373990731915",
+    
+    // Configurações do painel de tickets (padrões)
+    panelConfig: {
+        title: "🎫 Sistema de Tickets",
+        description: "Clique no botão abaixo para abrir um ticket e receber suporte!",
+        color: 5793266,
+        image: null
+    }
+};
+
+// Criado por davicsn
